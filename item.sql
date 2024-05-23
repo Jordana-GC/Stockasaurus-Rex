1c15d307d58a5a7d4212ffc5affacadc9ab482c2
daffrzkyand <daffarizkyananda1999@gmail.com>
2024-05-23 22:03:51 +0200

    Update item.sql

--- item.sql
+++ item.sql
@@ -15,4 +15,7 @@ INSERT INTO item(itemName, freshDay)
 VALUES ('eggs', '14')
 
 INSERT INTO item(itemName, freshDay)
-VALUES ('duiven', '3')
+VALUES ('banana', '3')
+
+INSERT INTO item(itemName, freshDay)
+VALUES ('apple', '6')
