f596b437394edf5dad19ea952d91e7bace410041
daffrzkyand <daffarizkyananda1999@gmail.com>
2024-05-23 22:03:29 +0200

    Update item_fridge.sql

--- item_fridge.sql
+++ item_fridge.sql
@@ -19,10 +19,10 @@ INSERT INTO item_fridge(fridgeNumber, itemName, freshDay, entryDate)
 VALUES ( 2, 'eggs', 14, '2024-05-17')
 
 INSERT INTO item_fridge(fridgeNumber, itemName, freshDay, entryDate)
-VALUES ( 2, 'fish meat', 5, '2024-05-16')
+VALUES ( 2, 'banana', 4, '2024-05-16')
 
 INSERT INTO item_fridge(fridgeNumber, itemName, freshDay, entryDate)
-VALUES ( 2, 'duiven', 3, '2024-05-19')
+VALUES ( 2, 'apple', 6, '2024-05-19')
 
 UPDATE item_fridge
 SET expiryDate = DATE(entryDate, '+' || freshDay || ' days');
