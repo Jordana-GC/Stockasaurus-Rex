04af5cf3ebf44618f3157c0936d8edc13afec402
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2024-05-24 11:37:49 +0200

    Added logo to nav bar, changed styling of pages

--- shelfscanners/src/components/NavBar.js
+++ shelfscanners/src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import Logo from "../assets/StockosaurusRex.png";
 
 const NavBar = () => {
     return (
         <div className="navbar">
-            <div className="logo">Stockosaurus Rex</div>
+            <div className="logo">
+                <img src={Logo} alt="logo" />
+            </div>
             <ul>
                 <li><Link to="/">Fridges</Link></li>
                 <li><Link to="/Notifications">Notifications</Link></li>
