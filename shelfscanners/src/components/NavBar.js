9fcad069461aa30152190dd629b58e4918e76025
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2024-05-27 17:28:49 +0200

    Made changes to the NavBar, added pngs and changed the logo

--- shelfscanners/src/components/NavBar.js
+++ shelfscanners/src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import Logo from "../assets/StockosaurusRex.png";
+import FridgeIcon from "../assets/fridge.png";
+import NotificationsIcon from "../assets/notifications.png";
 
 const NavBar = () => {
     return (
@@ -10,8 +12,18 @@ const NavBar = () => {
                 <img src={Logo} alt="logo" />
             </div>
             <ul>
-                <li><Link to="/fridges">Fridges</Link></li>
-                <li><Link to="/Notifications">Notifications</Link></li>
+                <li>
+                    <Link to="/fridges">
+                        <img src={FridgeIcon} alt="Fridge" className="nav-icon" />
+                        Fridges
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/notifications">
+                        <img src={NotificationsIcon} alt="Notifications" className="nav-icon" />
+                        Notifications
+                    </Link>
+                </li>
             </ul>
         </div>
     );
