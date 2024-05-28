223ffde4a6207557e9f4f7f535d3c728343c3d43
Jo-GC <jordana.guilbride.capela@student.nhlstenden.com>
2024-05-29 00:24:48 +0200

    Retrained the model with the new dataset

---
+++ train_test.py
@@ -0,0 +1,11 @@
+import torch
+from yolov5 import train 
+
+# Set parameters
+data = r'C:\Users\jorda\Desktop\Stockasaurus-Rex\dataset\data.yaml'  # path to your data.yaml file
+epochs = 100  # number of epochs to train
+batch_size = 16  # batch size
+img_size = 640  # image size
+
+# Train the model
+train.run(data=data, epochs=epochs, batch_size=batch_size, imgsz=img_size)
