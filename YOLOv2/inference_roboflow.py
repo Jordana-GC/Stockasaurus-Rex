7f31aad0993c376250ed4a217be654c85c7e70c7
Jo-GC <jordana.guilbride.capela@student.nhlstenden.com>
2024-05-28 11:39:23 +0200

    Added small testing changes

---
+++ YOLOv2/inference_roboflow.py
@@ -0,0 +1,3 @@
+#can specify weights_filename, default is "weights/best.pt"
+version = project.version(2)
+version.deploy("yolov8", r"C:\Users\jorda\Desktop\Stockasaurus-Rex\YOLOv2\yolov5\runs\train\exp3")
